04c1b9f5e7cdc6318581e30180a25800dc4a2919
Mark Rusciano <markrusciano@gmail.com>
2014-10-25 14:15:54 -0500

    Now communicates with rover over serial

--- TextModeControl/JoystickToSerial.py
+++ TextModeControl/JoystickToSerial.py
@@ -2,47 +2,51 @@ __author__ = 'Mark'
 
 import serial
 import pygame
-import time
 
 
 def main():
     pygame.init()
     pygame.joystick.init()
-    pygame.event.pump()
 
     clock = pygame.time.Clock()
 
-    ser = serial.Serial(6); #Virtual Serial Port for now
+    ser = serial.Serial('COM4', 57600); #Virtual Serial Port for now
 
     #print joystick info
-    joystick_count = pygame.joystick.get_count()
-    for i in range(joystick_count):
-        joystick = pygame.joystick.Joystick(i)
-        joystick.init()
-        print("Joystick {}".format(i))
-        print(joystick.get_name())
-
-        axes = joystick.get_numaxes()
-        print("Number of Axes: {}".format(axes))
+    # joystick_count = pygame.joystick.get_count()
+    # for i in range(joystick_count):
+    #     joystick = pygame.joystick.Joystick(i)
+    #     joystick.init()
+    #     print("Joystick {}".format(i))
+    #     print(joystick.get_name())
+    #
+    #     axes = joystick.get_numaxes()
+    #     print("Number of Axes: {}".format(axes))
 
     joystick = pygame.joystick.Joystick(0)
     joystick.init()
 
     while True:
-        clock.tick(1)
+        clock.tick(10)
         pygame.event.pump()
         joyX = -joystick.get_axis(0)
         joyY = -joystick.get_axis(1)
 
         leftJoyValue = joyY + joyX
         rightJoyValue = joyY - joyX
-        print(leftJoyValue)
-        print(rightJoyValue)
+        # print(leftJoyValue)
+        # print(rightJoyValue)
 
         leftPower = int(mapRange(leftJoyValue, -2, 2, -127, 127))
         rightPower = int(mapRange(rightJoyValue, -2, 2, -127, 127))
 
-        ser.write("{:0=+4d} {:0=+4d}\n".format(leftPower, rightPower))
+        packet = "<{:0=+4d} {:0=+4d}>".format(leftPower, rightPower)
+
+        ser.write(packet)
+        print(packet)
+
+        print(ser.readline())
+        #print(ser.readline())
 
 
 
